c923abda7678453ccc9820b084ca02fe7d63158c
David Pine <david.pine.7@gmail.com>
2024-10-01 10:18:47 -0500

    Add images with server-side proxy to avoid CORS issues

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.FluentUI.AspNetCore.Components;
+using System.Net.Http;
+using System;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -88,5 +91,14 @@ app.MapRazorComponents<App>()
 
 // Add additional endpoints required by the Identity /Account Razor components.
 app.MapAdditionalIdentityEndpoints();
+
+app.MapGet("api/proxy/image", async ([FromQuery] Uri url, IHttpClientFactory factory) =>
+{
+    var response = await factory.CreateClient().GetAsync(url);
+    var contentType = response.Content.Headers.ContentType?.ToString();
+    var content = await response.Content.ReadAsByteArrayAsync();
+
+    return Results.File(content, contentType);
+});
 
 app.Run();
