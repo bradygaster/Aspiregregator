0c462a2f6930d540c24fcf2b12e0c898a2dd68de
Brady Gaster <bradyg@microsoft.com>
2024-10-02 11:29:55 -0700

    deleted common project

--- Common/SourceItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Aspiregregator;
-
-public class SourceItem
-{
-    public required string Endpoint { get; set; }
-    public string Name { get; set; }
-    public DateTimeOffset LastUpdate { get; set; } = DateTimeOffset.MinValue;
-    public List<EntryItem> MostRecentItems { get; set; } = [];
-}
